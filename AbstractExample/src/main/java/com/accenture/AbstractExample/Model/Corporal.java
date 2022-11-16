780e08fc840a9065a6789d0da3b6b102ac7e8feb
fredditc <90579618+fredditc@users.noreply.github.com>
2022-11-16 09:37:26 -0600

    Update Corporal.java

--- AbstractExample/src/main/java/com/accenture/AbstractExample/Model/Corporal.java
+++ AbstractExample/src/main/java/com/accenture/AbstractExample/Model/Corporal.java
@@ -9,7 +9,7 @@ public class Corporal extends Military{
 
     @Override
     public void doJob() {
-        System.out.println("Corporal " + this.getName() + "'s job is to act as a liaison between recruits and soldiers.");
+        System.out.println("Corporal " + this.getName() + "'s job is to link as a liaison between recruits and soldiers.");
     }
 }
 
